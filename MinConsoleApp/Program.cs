0d723ff2e80bd02d6edaa43f1e57cb83b6014b90
Michell <michell@cronberg.dk>
2022-11-08 15:43:37 +0100

    .

--- MinConsoleApp/Program.cs
+++ MinConsoleApp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            // Global fejlhåndtering
+            // Global fejlhåndtering...
             try
             {
                 Run();
