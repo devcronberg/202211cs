0e6c3f1a6a89bf2c3b0110473a2db7c38b91c332
Michell <Admin@AR-112-INS>
2022-11-15 10:59:15 +0100

    .

---
+++ TerningBæger/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace TerningBæger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bæger b = new Bæger();
+            for (int i = 0; i < 10; i++)
+            {
+                b.Ryst();
+                b.Skriv();
+                Console.WriteLine(b.Point());
+            }
+        }
+    }
+
+    public class Bæger
+    {
+
+        Terning[] Terninger = new Terning[5];
+
+        public Bæger()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Terninger[i] = new Terning();
+            }
+        }
+
+        internal void Ryst()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Terninger[i].Ryst();
+            }
+        }
+
+        internal void Skriv()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Terninger[i].Skriv();
+            }
+            Console.WriteLine();
+        }
+
+        public bool ErYatzy()
+        {
+            // LINQ
+
+            return false;
+        }
+
+        public bool ErToPar()
+        {
+            // LINQ
+
+            return false;
+        }
+
+        public int Point()
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += Terninger[i].Værdi;
+            }
+            return sum;
+        }
+
+    }
+
+    public class Terning
+    {
+
+        /// <summary>
+        /// Initialisering
+        /// </summary>
+        public Terning()
+        {
+            this.Værdi = 1;
+        }
+
+        public int Værdi;   // 32 bit
+
+        /// <summary>
+        /// Skriver værdien ud
+        /// </summary>
+        public void Skriv()
+        {
+            Console.Write($"[ {this.Værdi} ] ");
+        }
+
+        /// <summary>
+        /// Tilfældig værdi
+        /// </summary>
+        public void Ryst()
+        {
+            Random rnd = new Random();
+            this.Værdi = rnd.Next(1, 7);
+        }
+    }
+
+    public class Spiller
+    {
+
+    }
+}
